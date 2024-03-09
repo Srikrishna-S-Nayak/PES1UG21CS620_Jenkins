35a798e6b2c4469b2d74a8af074ffd14652ebf02
Srikrishna S. Nayak <srikrishnasnayak@gmail.com>
2024-03-09 18:58:18 +0530

    Fixed the error in cpp file

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "Added a webhook in PES1UG21CS620_Jenkins repo";
+  cout << "Added a Webhook in PES1UG21CS620_Jenkins repository";
   cout << "I have successfully built and run\n";
   return 0;
 }
