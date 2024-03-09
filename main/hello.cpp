cb81a1d31445e5467661d904f96848bafebd443e
Srikrishna S. Nayak <srikrishnasnayak@gmail.com>
2024-03-09 19:01:12 +0530

    Added a cout

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "Added a Webhook in PES1UG21CS620_Jenkins repository";
+  cout << "Added a Webhook in PES1UG21CS620_Jenkins repos";
   cout << "I have successfully built and run\n";
   return 0;
 }
